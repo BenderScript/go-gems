ba364fb420237271c7a4c6ebb8d7ab0e9f63613b
repenno <rapenno@gmail.com>
2020-02-19 22:40:15 -0800

    Updated README.md

---
+++ closures/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	closureA := createClosure(1)
+	useClosure(2, closureA)
+	useClosure(5, closureA)
+}
+
+// createClosure creates two closures. One around a variable defined within the function
+// and another around a function parameter
+func createClosure(closureInput int) func(int) int {
+	fmt.Println("Create Closure")
+	closureVar := -10
+	fmt.Println(fmt.Sprintf("%-20v : %d", "closureInput ", closureInput))
+	fmt.Println(fmt.Sprintf("%-20v : %d", "closureVar ", closureVar))
+	return func(a int) int {
+		closureInput += a
+		closureVar -= a
+		fmt.Println(fmt.Sprintf("%-20v : %d", "closureInput ", closureInput))
+		fmt.Println(fmt.Sprintf("%-20v : %d", "closureVar ", closureVar))
+		return closureInput + closureVar
+	}
+}
+
+// useClosure takes a function with a closure var and use it.
+// It is a dramatic way of showing closures since this function has absolutely no
+// reference to the "closed" variable
+func useClosure(a int, f func(int) int) int {
+	fmt.Println("Use Closure, input: ", a)
+	return f(a)
+}
